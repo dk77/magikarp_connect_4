01388b6a9df1afb2458fd1ae4942f50da2ab1a88
Apprentice <davinrk@gmail.com>
2014-09-25 19:48:02 -0700

    some more updates

--- public/js/application.js
+++ public/js/application.js
@@ -1,37 +1,50 @@
 // var turn_counter = 0
 
 $(document).ready(function() {
+
   $("form").bind("click", function(evt) {
     evt.preventDefault();
     var that = this;
     // console.log(this)
-    // var turns = function(){
-    //   var count = $('#turn_counter').val();
-    //   if (count % 2 === 0){
-    //     return 'red'
-    //   } else  {
-    //     return 'black'
-    //   };
-    //   count++;
-    //   $('#turn_counter').val(count)
-    //   var row_count = function(){
-    //     var row = $('#row_count').val();
-    //     row++;
-    //     if (row === 6){
-    //       var button = $('column').val(hidden)
-    //     }
-    //   };
-    // };
+        var changeColor = function(data) {
+            var column = $(this).find('input[value]')[1].value;
+            var row_count = $("#row").val();
+          $('#'+ row_count + ' td:nth-child('+ column + ')').css('background-color', turns())
+          increaseRow()
+        }
+
+        var turns = function(){
+          var count = $('#turn_counter').val();
+              if (count % 2 === 0){
+                count++;
+                $('#turn_counter').val(count);
+                return 'red'
+              } else  {
+                count++;
+                $('#turn_counter').val(count);
+                return 'black'
+              };
+        };
+        var increaseRow = function(){
+            var row = $('#row').val();
+            row--;
+            $('#row').val(row);
+            if (row === 6){
+              var button = $('column').val(hidden);
+            }
+          };
+
+
     //   console.log($('#turn_counter').val())
     // // console.log(turn_counter);
     var column = $(that).find('input[value]')[1].value
-    console.log(column)
+    var row_count = $("#row").val();
+    var row_integer = parseInt(row_count)
+    // console.log(column)
     $.ajax({
       url:'/',
       type: 'POST',
-    }).done(function(data) {
-      $('#1 td:nth-child('+ column + ')').css('background-color', 'black')
-    })
+    }).done(changeColor.bind(that))
     .fail(function() {
 
     }).always(function(){
